4cb536d02a52bf3e62eabb94742c0dfb2fedd825
Azutech <azunna.onugha@gmail.com>
2024-03-01 14:09:47 +0100

    Unwanted files removed

--- src/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const auth_1 = require("./auth");
-exports.routes = (0, express_1.Router)();
-exports.routes.use('/user', auth_1.user);
--- src/utils/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SENDGRID_API_KEY = exports.JWT_SECRET = exports.DATABASE_URI = exports.PORT = void 0;
-exports.PORT = process.env.PORT;
-exports.DATABASE_URI = process.env.DATABASE_URI;
-exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
